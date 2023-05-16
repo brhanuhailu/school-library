d32024d6209e0472bd68544a2345669f790cd55b
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-16 06:58:13 -0700

    test.rb

--- test.rb
+++ test.rb
@@ -1,12 +1,23 @@
+require_relative './persons/teacher'
+require_relative './persons/student'
 require_relative './persons/person'
-require_relative './decorator/capitalize_decorator'
-require_relative './decorator/trimmer_decorator'
+require_relative './persons/rental'
+require_relative './persons/book'
+require_relative './persons/classroom'
 
-person = Person.new(22, 'maximilianus')
-puts person.correct_name
+mid_class = Classroom.new('Midclass')
 
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
+lily = Person.new(12, 'lily', parent_permission: false)
 
-capitalized_trimmed_person = TrimDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+student_lily = Student.new(mid_class, lily.age, lily.name, parent_permission: false)
+student_lily.add_classroom = mid_class
+
+fantasy1 = Book.new('The curse child', 'J.K.Rowling')
+fantasy2 = Book.new('Twilight saga', 'Stephenie Meyer')
+
+lily.add_rental(fantasy1, '2023-01-11')
+lily.add_rental(fantasy2, '2023-01-11')
+
+puts mid_class.students
+puts lily.rents
+puts fantasy1.rents
