2b446eb3c2d17ac140aaa5554a59163315d79e82
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-16 03:53:22 -0700

    fixed linters

--- decorator/trimmer_decorator.rb
+++ decorator/trimmer_decorator.rb
@@ -2,7 +2,7 @@ require_relative 'base'
 
 class TrimDecorator < Decorator
   def correct_name
-    super.slice(0, 9) + '...'
-    super[0..9] + ''
+    super.slice(0, 9)
+    (super[0..9]).to_s
   end
 end
