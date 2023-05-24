6d71a5891d10bdd12885e764318cfca90141cfbe
brhanuhailu <hailubrhanu29@gmail.com>
2023-05-24 07:29:05 -0700

    student.rb

--- persons/student.rb
+++ persons/student.rb
@@ -1,18 +1,20 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom, :parent_permission
+  attr_writer :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission:)
+  def initialize(age, name, parent_permission, classroom: 'Unknown')
+    super(age, name)
+    @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\\(ツ)/¯'
   end
-
-  def add_classroom=(label)
-    @classroom = label
-    classroom.students.push(self) unless classroom.students.include?(self)
+  def classroom=(classroom)
+    @classroom = classroom
+    @classroom.students << self unless @classroom.students.include?(self)
   end
 end
